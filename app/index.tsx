d65ac9ec51af5ee3f721898b8d06f982037bf555
Nati <nsisay49@gmail.com>
2026-02-08 20:35:45 +0300

    login auth

--- app/index.tsx
+++ app/index.tsx
@@ -3,9 +3,11 @@ import { Dimensions, View, Text, Image } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 import { Button } from "@/components/ui/button"
 import { Feather } from '@expo/vector-icons';
+import { useRouter } from "expo-router"
 
 const index = () => {
   const { height, width } = Dimensions.get("window")
+  const router = useRouter()
   return (
     <SafeAreaView style={{ backgroundColor: colors.background, height: "100%" }}>
       <View className="w-full h-full">
@@ -25,8 +27,15 @@ const index = () => {
         </View>
         <View className="px-5 pt-5">
           <Button
-            style={{ backgroundColor: colors.primary }}
+            style={{
+              backgroundColor: colors.primary,
+              shadowColor: "#000",
+              shadowOffset: { width: 0, height: 2 },
+              shadowOpacity: 0.7,
+              shadowRadius: 4,
+            }}
             className="border shadow-lg h-25"
+            onPress={() => router.push("/register")}
           >
             <Text
               className="text-white"
--- assets/colors.ts
+++ assets/colors.ts
@@ -1,2 +1,7 @@
-export const colors = { primary: "#5854A3", secondary: "#FF6B50", typo: "#000000", background: "#FFF9E8" }
+export const colors = {
+  primary: "#5854A3",
+  secondary: "#FF6B50",
+  typo: "#000000",
+  background: "#FFF9E8"
+}
 
